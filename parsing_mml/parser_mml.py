00b4f070051c9d109ebc1a1900a9c537a7de48a9
ghost <ghost@mail.com>
2020-07-15 14:51:27 -0400

    Input file detection

--- parsing_mml/parser_mml.py
+++ parsing_mml/parser_mml.py
@@ -1,8 +1,18 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 
-def main():
-    print('Hello world !!!')
+# Usage: ./parser_mml.py [-h] input
+
+import argparse
+
+def mml_parser(file_path):
+    print(file_path)
+
 
 if __name__ == '__main__':
-    main()
+    parser = argparse.ArgumentParser(description='File to process')
+    parser.add_argument('input', type=str, help='Input file')
+    # parser.add_argument('output', type=str, help='Output file')
+    args = parser.parse_args()
+
+    mml_parser(args.input)
