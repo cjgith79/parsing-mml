8351dcbc61387fdf1f629c5bb2382adc54be995e
ghost <ghost@mail.com>
2020-07-15 16:04:33 -0400

    Reading lines

--- parsing_mml/parser_mml.py
+++ parsing_mml/parser_mml.py
@@ -7,7 +7,11 @@ import argparse
 
 def mml_parser(file_path):
     print(file_path)
-
+    count = 0
+    with open(file_path) as fp:
+        for line in fp:
+            count += 1
+            print("Line{}: {}".format(count, line.strip()))
 
 if __name__ == '__main__':
     parser = argparse.ArgumentParser(description='File to process')
