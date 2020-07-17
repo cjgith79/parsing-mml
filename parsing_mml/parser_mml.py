1f9bf9cf11b6a5443c9aa2ddcae3ef88e654c449
ghost <ghost@mail.com>
2020-07-17 14:58:34 -0400

    Recognizing command

--- parsing_mml/parser_mml.py
+++ parsing_mml/parser_mml.py
@@ -11,14 +11,36 @@ def mml_parser(file_path):
     # print(file_path)
     count = 0
     # ----------------------
-    resul_mml = ResultMML()
+    result_mml = ResultMML(None)
     # ----------------------
     with open(file_path) as fp:
         for line in fp:
             # -------------------------------------------------------
-            resul_mml.command_search(line)
-            if resul_mml.command:
-                print(f"command[{resul_mml.command}]")
+            # result_mml.command_search(line, count)
+            # if result_mml.command:
+            #     print(f"command[{result_mml.command}]")
+
+            # if not result_mml.command: # command not detected yet
+            #     result_mml.command_search(line, count)
+            #     if result_mml.command:
+            #         print(f"command[{result_mml.command}]")
+            # else:
+
+            '''
+            Siempre debo mirar por "MML Command-----"
+            Si se detecta
+                Si ya estaba lleno ->
+                    devolver el objeto (yield ?)
+                    reiniciar objeto con nuevo comando detectado
+                Si objeto.ne está vacío
+                    ver si se detecta ne
+                ...
+            '''
+            command_ = result_mml.command_search(line)
+            if command_:
+                print(result_mml) # yield maybe
+                result_mml = ResultMML(command_)
+
             # -------------------------------------------------------
             count += 1
             print("Line{}: {}".format(count, line.strip()))
--- parsing_mml/result_mml.py
+++ parsing_mml/result_mml.py
@@ -6,18 +6,35 @@ import re
 class ResultMML(object):
     """docstring for ResultMML"""
     pattern_command = re.compile('MML Command-----(.*)')
+    pattern_ne = re.compile('NE : (.*)')
+    # pattern_ne = re.compile('Report : +++    (.*)')
+    # pattern_ne = re.compile('Report : +++    (.*?)')
+    # 'Report : +++    (.*?)(\d{4}-\d{2}-\d{2} \d{2}:\d{2}:\d{2})')
 
-    def __init__(self):
-    # def __init__(self, line_number):
+    def __init__(self, command):
         super(ResultMML, self).__init__()
-        # self.line_number = line_number
-        self.command = None
+        # self.command_line_number = None
+        self.command = command
         self.ne = None
         self.executed_time_stamp = None
         self.o_m_id = None
         self.result = None
 
     def command_search(self, line):
+        '''
+            If match
+                returns command
+            ELSE
+                returns false
+        '''
+        # def command_search(self, line, line_number):
         match = ResultMML.pattern_command.search(line)
         if match:
-            self.command = match.group(1)
+            return match.group(1)
+        return False
+
+    def __str__(self):
+        message = (
+                f"command:[{self.command}]\n"
+            )
+        return message
