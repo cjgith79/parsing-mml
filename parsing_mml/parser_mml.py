b4e9dbafe4a91897820c861714f0f7a589358f8d
ghost <ghost@mail.com>
2020-07-17 15:33:37 -0400

    Recognizing ne

--- parsing_mml/parser_mml.py
+++ parsing_mml/parser_mml.py
@@ -16,30 +16,14 @@ def mml_parser(file_path):
     with open(file_path) as fp:
         for line in fp:
             # -------------------------------------------------------
-            # result_mml.command_search(line, count)
-            # if result_mml.command:
-            #     print(f"command[{result_mml.command}]")
 
-            # if not result_mml.command: # command not detected yet
-            #     result_mml.command_search(line, count)
-            #     if result_mml.command:
-            #         print(f"command[{result_mml.command}]")
-            # else:
-
-            '''
-            Siempre debo mirar por "MML Command-----"
-            Si se detecta
-                Si ya estaba lleno ->
-                    devolver el objeto (yield ?)
-                    reiniciar objeto con nuevo comando detectado
-                Si objeto.ne está vacío
-                    ver si se detecta ne
-                ...
-            '''
             command_ = result_mml.command_search(line)
             if command_:
                 print(result_mml) # yield maybe
                 result_mml = ResultMML(command_)
+
+            result_mml.fields_search(line)
+
 
             # -------------------------------------------------------
             count += 1
--- parsing_mml/result_mml.py
+++ parsing_mml/result_mml.py
@@ -21,20 +21,26 @@ class ResultMML(object):
         self.result = None
 
     def command_search(self, line):
-        '''
-            If match
-                returns command
-            ELSE
-                returns false
-        '''
-        # def command_search(self, line, line_number):
         match = ResultMML.pattern_command.search(line)
         if match:
             return match.group(1)
         return False
 
+    def ne_search(self, line):
+        if not self.ne:
+            match = ResultMML.pattern_ne.search(line)
+            if match:
+                self.ne = match.group(1)
+
+    def fields_search(self, line):
+        self.ne_search(line)
+
     def __str__(self):
         message = (
                 f"command:[{self.command}]\n"
+                f"ne:[{self.ne}]\n"
+                f"executed_time_stamp:[{self.executed_time_stamp}]\n"
+                f"o_m_id:[{self.o_m_id}]\n"
+                f"result:[{self.result}]\n"
             )
         return message
